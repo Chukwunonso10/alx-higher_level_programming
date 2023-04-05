0b0892e06e719e8bfbdde77e3d8bce9c61da8ba4
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-04-05 05:08:12 -0700

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defining  a locked class"""
+
+
+class LockedClass:
+    """
+    This allows creation of intances of the class first_name
+    """
+
+    __slots__ = ["first_name"]
+
