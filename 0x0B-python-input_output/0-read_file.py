b5bea6129774443243daa7b0b22a77419c49d79c
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-04-12 18:54:55 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a text file-reading function"""
 
 def read_file(filename=""):
-    """Prints and opens a file in read mode """
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+        print(f.read(),end="")
