46e262d8f5dba3ed5284e1266259cc7eed74cf98
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-05-16 15:04:30 -0700

    2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
---If the database hbtn_0c_0 doesn’t exist, your script should not fail
---You are not allowed to use the SELECT or SHOW statements
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
